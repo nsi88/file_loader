058a77b06808b2bd1c822b382adc380dfc5e09f1
novikov <novikov@inventos.ru>
2015-09-15 19:03:21 +0300

    refactoring

--- lib/file_loader.rb
+++ lib/file_loader.rb
@@ -1,10 +1,26 @@
 require "file_loader/version"
 require 'shellwords'
 require 'socket'
-require 'fileutils'
+require 'logger'
 
 module FileLoader
   extend self
+  
+  # === Options
+  # [:permissions] b.e. '0777'
+  # [:speed_limit] in Kbit/s
+  # [:user] default user, used if url has no user
+  # [:password]
+  # [:logger] Logger instance
+  # [:retries] count of retries, default 1
+  # [:delay] sleep seconds between retries
+  # [:dry_run]
+  attr_accessor :defaults
+  self.defaults = {
+    retries: 0,
+    logger: Logger.new(STDOUT),
+    delay: 5
+  }
 
   def parse_url(url)
     url.match(/^(?:(?<protocol>\w+):\/\/(?:(?<user>.+?)(?:\:(?<password>.+?))?@)?(?<host>.+?):?)?(?<path>\/.+)?$/) or raise("Invalid url #{url}")
@@ -16,103 +32,106 @@ module FileLoader
     res += opts[:user].to_s
     res += ":#{opts[:password]}" if opts[:password]
     res += '@' if opts[:user]
-    res += "#{opts[:host]}:" if opts[:host]
-    res += opts[:path].to_s
+    res += opts[:host] if opts[:host]
+    res += ':' if opts[:host] && opts[:path]
+    res += opts[:path] if opts[:path]
+    res
   end
 
   def download(src, dst, opts = {})
-    src = src.shellescape
-    dst = dst.shellescape
-    psrc = parse_url(src)
-    unless Dir.exists?(File.dirname(dst))
-      FileUtils.mkdir_p(File.dirname(dst), :mode => opts[:permissions] ? opts[:permissions].to_i(8) : nil)
-    end
+    pdst = parse_url(dst = dst.shellescape)
+    raise "Unsupported dst protocol #{pdst[:protocol]}" if pdst[:protocol]
+    
+    opts = defaults.merge(opts)
+
+    psrc = parse_url(src = src.shellescape)
     case psrc[:protocol]
     when 'http', 'https', 'ftp'
       cmd = "wget #{src} -O #{dst}"
+      cmd += " --limit-rate #{opts[:speed_limit] * 1024 / 8}" if opts[:speed_limit]
     when 'scp'
       if Socket.gethostname == psrc[:host]
         cmd = cp_cmd(psrc[:path], dst)
       else
         cmd = "scp -r -oBatchMode=yes -oStrictHostKeyChecking=no "
-        cmd += "-l #{speed_limit} " if opts[:speed_limit]
+        cmd += "-l #{opts[:speed_limit]} " if opts[:speed_limit]
         cmd += '"'
         cmd += build_url(user: psrc[:user] || opts[:user], password: psrc[:password] || opts[:password], host: psrc[:host], path: psrc[:path])
+        cmd += '" "'
+        cmd += dst
         cmd += '"'
-        cmd += " \"#{dst}\""
       end
     when nil
       cmd = cp_cmd(src, dst)
     else
-      raise "Unsupported protocol #{psrc[:protocol]}"
+      raise "Unsupported src protocol #{psrc[:protocol]}"
     end
-    opts[:logger].debug(cmd) if opts[:logger]
-    exec_cmd(cmd, opts[:retries].to_i) unless opts[:dry_run]
+    cmd = mkdir_cmd(dst, opts) + ' && ' + cmd if cmd
+    exec_cmd(cmd, opts)
   end
 
   def upload(src, dst, opts = {})
-    src = src.shellescape
-    dst = dst.shellescape
-    parsed_dst = parse_url(dst)
-    case parsed_dst[:protocol]
+    psrc = parse_url(src = src.shellescape)
+    raise "Unsupported src protocol #{psrc[:protocol]}" if psrc[:protocol]
+  
+    opts = defaults.merge(opts)
+      
+    pdst = parse_url(dst = dst.shellescape)
+    case pdst[:protocol]
     when 'scp'
-      if Socket.gethostname == parsed_dst[:host]
-        unless Dir.exists?(File.dirname(dst))
-          FileUtils.mkdir_p(File.dirname(dst), :mode => opts[:permissions] ? opts[:permissions].to_i(8) : nil)
-        end
-        cmd = cp_cmd(src, parsed_dst[:path])
+      if Socket.gethostname == pdst[:host]
+        cmd = cp_cmd(src, pdst[:path])
+        cmd = mkdir_cmd(pdst[:path], opts) + ' && ' + cmd if cmd
       else
         cmd = "ssh -oBatchMode=yes -oStrictHostKeyChecking=no "
-        user = parsed_dst[:user] || opts[:user]
-        cmd += user if user
-        cmd += ":#{parsed_dst[:password]}" if parsed_dst[:password]
-        cmd += '@' if user
-        cmd += "#{parsed_dst[:host]} \""
-        if opts[:permissions]
-          umask = 'umask %03o' % ('0777'.to_i - opts[:permissions].to_i)
-          cmd += "(#{umask}; "
-        end
-        cmd += "mkdir -p #{File.dirname(parsed_dst[:path])}"
-        cmd += ")" if opts[:permissions]
-        cmd += "\" && scp -r -oBatchMode=yes -oStrictHostKeyChecking=no "
+        cmd += build_url(user: pdst[:user] || opts[:user], password: pdst[:password], host: pdst[:host])
+        cmd += ' "'
+        cmd += mkdir_cmd(pdst[:path], opts)
+        cmd += '" && scp -r -oBatchMode=yes -oStrictHostKeyChecking=no '
         cmd += "-l #{opts[:speed_limit]} " if opts[:speed_limit]
         cmd += "#{src} \""
-        cmd += user if user
-        cmd += ":#{opts[:password]}" if opts[:password]
-        cmd += '@' if user
-        cmd += "#{parsed_dst[:host]}:#{parsed_dst[:path]}\""
+        cmd += build_url(user: pdst[:user] || opts[:user], password: pdst[:password], host: pdst[:host], path: pdst[:path])
+        cmd += '"'
       end
     when nil
-      unless Dir.exists?(File.dirname(dst))
-        FileUtils.mkdir_p(File.dirname(dst), :mode => opts[:permissions] ? opts[:permissions].to_i(8) : nil)
-      end
       cmd = cp_cmd(src, dst)
+      cmd = mkdir_cmd(pdst[:path], opts) + ' && ' + cmd if cmd
     else
       raise "Unsupported protocol #{parsed_src[:protocol]}"
     end
-    opts[:logger].debug(cmd) if opts[:logger]
-    exec_cmd(cmd, opts[:retries].to_i) unless opts[:dry_run]
+    exec_cmd(cmd, opts)
   end
 
   private
 
-  def exec_cmd(cmd, retries = 0, delay = 5)
-    return unless cmd
+  def exec_cmd(cmd, opts)
+    opts[:logger].debug(cmd) if opts[:logger]
+    return if opts[:dry_run] || !cmd
     cmd += " 2>&1"
-    (retries + 1).times do |n|
+    (opts[:retries].to_i + 1).times do |n|
       res = `#{cmd}`
       if $?.exitstatus == 0
         break
-      elsif n >= retries
+      elsif n >= opts[:retries].to_i
         raise res
       else
-        sleep delay
+        opts[:logger].debug('retry') if opts[:logger]
+        sleep(opts[:delay] || 5)
       end
     end
   end
 
-  def cp_cmd(src, dst, permissions = nil)
+  def cp_cmd(src, dst)
     return if src == dst
     "cp -r #{src} #{dst}"
+  end
+
+  def mkdir_cmd(dst, opts)
+    cmd = "mkdir -p #{File.dirname(dst)}"
+    if opts[:permissions]
+      umask = 'umask %03o' % ('0777'.to_i - opts[:permissions].to_i)
+      cmd = "(#{umask}; #{cmd})"
+    end
+    cmd
   end
 end
--- lib/file_loader/version.rb
+++ lib/file_loader/version.rb
@@ -1,3 +1,3 @@
 module FileLoader
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
